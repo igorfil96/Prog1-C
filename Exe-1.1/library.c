cd5fe737ca29dd32b32cb56b3e0b35f4dab931d5
igorfil96 <igorfil1979@gmail.com>
2026-03-29 22:21:11 +0100

    Exe1.1

---
+++ Exe-1.1/library.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello world!\n");
+
+    printf("\tExemplo de uma tabulação\n");
+
+    int numero;
+
+    printf("Por favor, introduza um número: ");
+    scanf("%d", &numero;
+
+    printf("O valor que introduziu foi: %d\n", numero);
+
+    return 0;
+}
---
+++ Exe-1.1/library.h
@@ -0,0 +1,6 @@
+#ifndef PROG1_C_LIBRARY_H
+#define PROG1_C_LIBRARY_H
+
+void hello(void);
+
+#endif // PROG1_C_LIBRARY_H
